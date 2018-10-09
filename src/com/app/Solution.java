8aaaa8858959244dd1909dd1b8cf69cb6f9e671a
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2018-10-09 14:13:54 +0300

    Add a working solution

--- src/com/app/Solution.java
+++ src/com/app/Solution.java
@@ -3,7 +3,16 @@ package com.app;
 public class Solution {
 
     public int hammingDistance(int x, int y) {
-        int res = x ^ y;
+        int xor = x ^ y;
+        int res = 0;
+        int bit = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((bit & xor) != 0) {
+                res++;
+            }
+            bit <<= 1;
+        }
+
         return res;
     }
 }
